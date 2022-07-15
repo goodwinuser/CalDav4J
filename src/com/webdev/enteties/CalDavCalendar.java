6a940ffdd867817345f224b5154b311b203194c4
Zagaynov Simeon <devehher@gmail.com>
2022-07-15 17:12:31 +0300

    Добавление сущностей календаря и события

---
+++ src/com/webdev/enteties/CalDavCalendar.java
@@ -0,0 +1,134 @@
+package com.webdev.enteties;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
+
+public class CalDavCalendar {
+
+    private String uid;
+
+    private URL calendarUrl;
+
+    private String displayName;
+
+    private String description;
+
+    private String owner;
+
+    private String productId;
+
+    private String syncToken;
+
+    private String color;
+
+    private String ctag;
+
+    private TimeZone timeZone;
+
+    private Calendar lastModified;
+
+    private List<CalDavEvent> events;
+
+    public CalDavCalendar() {
+        this.events = new ArrayList<>();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public URL getCalendarUrl() {
+        return calendarUrl;
+    }
+
+    public void setCalendarUrl(URL calendarUrl) {
+        this.calendarUrl = calendarUrl;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getSyncToken() {
+        return syncToken;
+    }
+
+    public void setSyncToken(String syncToken) {
+        this.syncToken = syncToken;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCtag() {
+        return ctag;
+    }
+
+    public void setCtag(String ctag) {
+        this.ctag = ctag;
+    }
+
+    public TimeZone getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(TimeZone timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public Calendar getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Calendar lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public List<CalDavEvent> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<CalDavEvent> events) {
+        this.events = events;
+    }
+}
---
+++ src/com/webdev/enteties/CalDavEvent.java
@@ -0,0 +1,115 @@
+package com.webdev.enteties;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class CalDavEvent {
+
+    private String uid;
+
+    private java.util.Calendar start;
+
+    private java.util.Calendar end;
+
+    private java.util.Calendar created;
+
+    private java.util.Calendar lastModified;
+
+    private String location;
+
+    private String summary;
+
+    private String description;
+
+    private List<String> members;
+
+    public CalDavEvent()
+    {
+        this.created = java.util.Calendar.getInstance();
+        this.lastModified = java.util.Calendar.getInstance();
+        this.members = new ArrayList<>();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Calendar getStart() {
+        return start;
+    }
+
+    public void setStart(Calendar start) {
+        this.start = start;
+    }
+
+    public Calendar getEnd() {
+        return end;
+    }
+
+    public void setEnd(Calendar end) {
+        this.end = end;
+    }
+
+    public Calendar getCreated() {
+        return created;
+    }
+
+    public void setCreated(Calendar created) {
+        this.created = created;
+    }
+
+    public Calendar getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Calendar lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+
+    public Boolean addMember(String memberEmail) {
+        this.lastModified = java.util.Calendar.getInstance();
+        return this.members.add(memberEmail);
+    }
+
+    public Boolean removeMember(String memberEmail) {
+        var status = this.members.remove(memberEmail);
+        if (status) {
+            this.lastModified = java.util.Calendar.getInstance();
+        }
+        return status;
+    }
+
+}
