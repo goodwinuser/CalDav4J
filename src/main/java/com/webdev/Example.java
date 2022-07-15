1270a3729aa553a6a21be7c87f0eebbc33338c7d
Zagaynov Simeon <devehher@gmail.com>
2022-07-15 19:24:14 +0300

    Полноценный функционал библиотеки, требующий тестирования

---
+++ src/main/java/com/webdev/Example.java
@@ -0,0 +1,22 @@
+package com.webdev;
+
+import com.webdev.enteties.CalDavEvent;
+import com.webdev.services.CalDavCalendarClient;
+
+import java.net.URL;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class Example {
+    public static void main(String[] args){
+
+        try {
+
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+}
