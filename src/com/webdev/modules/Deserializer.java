b276b44702adacdfc70671f6400f579d0a615ba1
Zagaynov Simeon <devehher@gmail.com>
2022-07-15 17:36:58 +0300

    Добавление серилизатора и десерилизатора

---
+++ src/com/webdev/modules/Deserializer.java
@@ -0,0 +1,250 @@
+package com.webdev.modules;
+
+import com.webdev.enteties.CalDavCalendar;
+import com.webdev.enteties.CalDavEvent;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
+
+public class Deserializer {
+    public static CalDavCalendar DeserializeCalendar(String source, CalDavCalendar calendar) throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        Document document;
+
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            document = builder.parse(new InputSource(new StringReader(source)));
+        }
+        catch(SAXException | ParserConfigurationException exception){
+            throw new Exception("Wrong calendar xml data");
+        }
+
+        var content = document.getElementsByTagName("href").item(0).getTextContent();
+        if (content != null && !content.equals("")) {
+            calendar.setUid(content);
+        } else {
+            calendar.setUid("");
+        }
+
+        content = document.getElementsByTagName("D:displayname").item(0).getTextContent();
+        if (content != null && !content.equals("")) {
+            calendar.setDisplayName(content);
+        } else {
+            calendar.setDisplayName("");
+        }
+
+        content = document.getElementsByTagName("calendar-color").item(0).getTextContent();
+        if (content != null && !content.equals("")) {
+            calendar.setColor(content);
+        } else {
+            calendar.setColor("");
+        }
+
+        content = document.getElementsByTagName("D:owner").item(0).getTextContent();
+        if (content != null && !content.equals("")) {
+            calendar.setOwner(content);
+        } else {
+            calendar.setOwner("");
+        }
+
+        content = document.getElementsByTagName("D:sync-token").item(0).getTextContent();
+        if (content != null && !content.equals("")) {
+            calendar.setSyncToken(content.replace("sync-token:", ""));
+        } else {
+            calendar.setSyncToken("");
+        }
+
+        content = document.getElementsByTagName("getctag").item(0).getTextContent();
+        if (content != null && !content.equals("")) {
+            calendar.setCtag(content);
+        } else {
+            calendar.setCtag("");
+        }
+
+        var events = document.getElementsByTagName("C:calendar-data");
+        var deserializedEvents = new ArrayList<CalDavEvent>();
+        for (int i = 0; i < events.getLength(); i++) {
+            deserializedEvents.add(
+                    DeserializeEvent(events.item(i).getTextContent(), calendar.getTimeZone())
+            );
+        }
+        calendar.setEvents(deserializedEvents);
+
+        return calendar;
+    }
+
+
+    public static CalDavEvent DeserializeEvent(String source, TimeZone timeZone) {
+
+        CalDavEvent targetEvent = new CalDavEvent();
+
+        var items = Arrays
+                .stream(source.split("\n"))
+                .dropWhile(x -> !x.equals("BEGIN:VEVENT"))
+                .map(x -> x.replaceAll(";\\S*:", ":")).toList();
+
+
+        var item = items.stream()
+                .filter(x -> x.contains("DTSTART:"))
+                .findFirst();
+        if (item.isPresent()) {
+            targetEvent.setStart(
+                    GetDateTime(
+                            item.get()
+                                    .replace("DTSTART:", ""),
+                            timeZone
+                    )
+            );
+        } else {
+            targetEvent.setStart(new GregorianCalendar());
+        }
+
+
+        item = items.stream()
+                .filter(x -> x.contains("DTEND:"))
+                .findFirst();
+        if (item.isPresent()) {
+            targetEvent.setEnd(
+                    GetDateTime(
+                            item.get()
+                                    .replace("DTEND:", ""),
+                            timeZone
+                    )
+            );
+        } else {
+            targetEvent.setEnd(new GregorianCalendar());
+        }
+
+        item = items.stream()
+                .filter(x -> x.contains("SUMMARY:"))
+                .findFirst();
+        if (item.isPresent()) {
+            targetEvent.setSummary(
+                    item.get()
+                            .replace("SUMMARY:", "")
+            );
+        } else {
+            targetEvent.setSummary("");
+        }
+
+        item = items.stream()
+                .filter(x -> x.contains("UID:"))
+                .findFirst();
+        if (item.isPresent()) {
+            targetEvent.setUid(
+                    item.get()
+                            .replace("UID:", "")
+            );
+        } else {
+            targetEvent.setUid("");
+        }
+
+        item = items.stream()
+                .filter(x -> x.contains("CREATED:"))
+                .findFirst();
+        if (item.isPresent()) {
+            targetEvent.setCreated(
+                    GetDateTime(
+                            item.get()
+                                    .replace("CREATED:", ""),
+                            timeZone
+                    )
+            );
+        } else {
+            targetEvent.setCreated(new GregorianCalendar());
+        }
+
+        item = items.stream()
+                .filter(x -> x.contains("LAST-MODIFIED:"))
+                .findFirst();
+        if (item.isPresent()) {
+            targetEvent.setLastModified(
+                    GetDateTime(
+                            item.get()
+                                    .replace("LAST-MODIFIED:", ""),
+                            timeZone
+                    )
+            );
+        } else {
+            targetEvent.setLastModified(new GregorianCalendar());
+        }
+
+        item = items.stream()
+                .filter(x -> x.contains("LOCATION:"))
+                .findFirst();
+        if (item.isPresent()) {
+            targetEvent.setLocation(
+                    item.get()
+                            .replace("LOCATION:", "")
+            );
+        } else {
+            targetEvent.setLocation("");
+        }
+
+        item = items.stream()
+                .filter(x -> x.contains("DESCRIPTION:"))
+                .findFirst();
+        if (item.isPresent()) {
+            targetEvent.setDescription(
+                    item.get()
+                            .replace("DESCRIPTION:", "")
+            );
+        } else {
+            targetEvent.setDescription("");
+        }
+
+        var allItems = items.stream()
+                .filter(x -> x.contains("ATTENDEE;PARTSTAT=NEEDS-ACTION;CN="))
+                .map(x -> x.replace("ATTENDEE;PARTSTAT=NEEDS-ACTION;CN=", ""))
+                .collect(Collectors.toList());
+        if (allItems.size() > 0) {
+            for (var element : allItems) {
+                var parts = element.split(":");
+
+                if (parts.length == 3) {
+                    targetEvent.addMember(parts[2]);
+                }
+            }
+        }
+
+        return targetEvent;
+    }
+
+    private static java.util.Calendar GetDateTime(String time, TimeZone timeZone) {
+
+        var result = new GregorianCalendar(timeZone);
+
+        int year = Integer.parseInt(time.substring(0, 4));
+        int month = Integer.parseInt(time.substring(4, 6));
+        int day = Integer.parseInt(time.substring(6, 8));
+
+        int hour = Integer.parseInt(time.substring(9, 11));
+        int minutes = Integer.parseInt(time.substring(11, 13));
+        int seconds = Integer.parseInt(time.substring(13, 15));
+
+
+        result.set(GregorianCalendar.SECOND, seconds);
+        result.set(GregorianCalendar.MINUTE, minutes);
+        result.set(GregorianCalendar.HOUR_OF_DAY, hour);
+        result.set(GregorianCalendar.DAY_OF_MONTH, day);
+        result.set(GregorianCalendar.MONTH, month);
+        result.set(GregorianCalendar.YEAR, year);
+
+        Integer timeZoneOffsetInHours = timeZone.getOffset(result.getTimeInMillis()) / (60 * 60 * 1000);
+        result.set(java.util.Calendar.HOUR_OF_DAY, result.get(java.util.Calendar.HOUR_OF_DAY) + timeZoneOffsetInHours);
+
+        return result;
+    }
+}
---
+++ src/com/webdev/modules/Serializer.java
@@ -0,0 +1,70 @@
+package com.webdev.modules;
+
+import com.webdev.enteties.CalDavEvent;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class Serializer {
+    public static String SerializeEventToString(CalDavEvent targetEvent, TimeZone timeZone, String login) {
+        var result = new StringBuilder("BEGIN:VCALENDAR\r\n");
+        result.append("BEGIN:VEVENT").append("\r\n");
+        result.append("DTEND:").append(SerializeTimeToString(targetEvent.getEnd(), timeZone)).append("\r\n");
+        result.append("DTSTART:").append(SerializeTimeToString(targetEvent.getStart(), timeZone)).append("\r\n");
+        result.append("CREATED:").append(SerializeTimeToString(targetEvent.getCreated(), timeZone)).append("\r\n");
+        result.append("LAST-MODIFIED:").append(SerializeTimeToString(targetEvent.getLastModified(), timeZone)).append("\r\n");
+        result.append("SEQUENCE:0").append("\r\n");
+        result.append("SUMMARY:").append(targetEvent.getSummary()).append("\r\n");
+        result.append("UID:").append(targetEvent.getUid()).append("\r\n");
+        result.append("LOCATION:").append(targetEvent.getLocation()).append("\r\n");
+        result.append("DESCRIPTION:").append(targetEvent.getDescription()).append("\r\n");
+        result.append("ORGANIZER;CN=").append(login.split("@")[0]).append(":mailto:").append(login).append("\r\n");
+        for(var pt : targetEvent.getMembers()){
+            result.append("ATTENDEE;PARTSTAT=NEEDS-ACTION;CN=")
+                    .append(pt.split("@")[0])
+                    .append(":mailto:")
+                    .append(pt)
+                    .append("\r\n");
+        }
+        result.append("END:VEVENT").append("\r\n");
+        result.append("END:VCALENDAR").append("\r\n");
+
+        return result.toString();
+    }
+
+    private static String SerializeTimeToString(Calendar dt, TimeZone timeZone) {
+
+        //convert local time to UTC format
+        var timeZoneOffsetInHours = timeZone.getRawOffset()/(60 * 60 * 1000);
+        dt.set(Calendar.HOUR_OF_DAY, dt.get(Calendar.HOUR_OF_DAY) - timeZoneOffsetInHours);
+
+        String year = String.valueOf(dt.get(Calendar.YEAR));
+
+        int mText = dt.get(Calendar.MONTH) + 1;
+        String month = String.valueOf(mText);
+        if (mText < 10) {
+            month = "0" + month;
+        }
+
+        String day = String.valueOf(dt.get(Calendar.DAY_OF_MONTH));
+        if (dt.get(Calendar.DAY_OF_MONTH) < 10) {
+            day = "0" + day;
+        }
+
+        String hour = String.valueOf(dt.get(Calendar.HOUR_OF_DAY));
+        if (dt.get(Calendar.HOUR_OF_DAY) < 10) {
+            hour = "0" + hour;
+        }
+
+        String minute = String.valueOf(dt.get(Calendar.MINUTE));
+        if (dt.get(Calendar.MINUTE) < 10) {
+            minute = "0" + minute;
+        }
+
+        String second = String.valueOf(dt.get(Calendar.SECOND));
+        if (dt.get(Calendar.SECOND) < 10) {
+            second = "0" + second;
+        }
+        return year + month + day + "T" + hour + minute + second + "Z";
+    }
+}
